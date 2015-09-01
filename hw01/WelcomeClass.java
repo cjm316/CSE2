4392b273eb24bf34d650269a5d5a5e8dd9f01d47
cjm316 <cjm316@lehigh.edu>
2015-09-01 22:36:02 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Homework 1
+//Welcome Class Java Program
+//
+//   first compile the program
+//      javac WelcomeClass.java
+//   then run the program
+//      java WelcomeClass.java//
+
+//   define a class
+public class WelcomeClass{
+    
+//   add main method
+  public static void main(String[] args) {
+      
+//   print the statement with the Welcome box, the correct characters, and the blurb at the end
+     System.out.println("  -----------        ");
+     System.out.println("  | WELCOME |        ");
+     System.out.println("  -----------        ");
+     System.out.println("  ^  ^  ^  ^  ^  ^   ");
+     System.out.println(" / \/ \/ \/ \/ \/ \  ");
+     System.out.println("|-C--J--M--3--1--6-| ");
+     System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /  ");
+     System.out.println("  v  v  v  v  v  v   ");
+     System.out.println("This past summer I worked for a car battery manufacturing");
+     System.out.println("company that sold products to companies like BMW and Mercedes Benz.");
+     
+     
+     //This is to test
+  }
+}
